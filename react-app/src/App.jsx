203b89a6f851271d2ea986588a0b442dcf4a9258
Dan1loJos <157768499+Dan1loJos@users.noreply.github.com>
2024-07-12 15:48:59 -0600

    menu de login

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,57 +2,46 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import ulatina from "/logo_ulatina.png";
 import "./App.css";
-import Box from "@mui/material/Box";
-import FormControl from "@mui/material/FormControl";
-import InputAdornment from "@mui/material/InputAdornment";
-import TextField from "@mui/material/TextField";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+
       <div>
-        <a
-          href="https://www.ulatina.ac.cr/sites/default/files/logo_ulatina_1_0.png"
-          target="_blank"
-        >
+        <a href="http://localhost:5174" target="_blank">
           <img src={ulatina} className="logo" alt="Vite logo" />
         </a>
-      </div>
 
-      <div>
-        <h2>Adninistración</h2>
-        <h2>LINTEC - PES - TFG</h2>
-      </div>
-
-      <div className="card">
-        <Box sx={{ "& > :not(style)": { m: 1 } }}>
-          <TextField
-            id="input-with-icon-textfield"
-            label="TextField"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-          />
-        </Box>
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>Administración</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <h2>LINTEC - PES - TFG</h2>
+        </p>
+
+        <p>
+          <TextField label="Usuario" id="textFieldUsuario" variant="standard" />
+        </p>
+        <p>
+          <TextField
+            label="Contraseña"
+            id="textFieldContrasena"
+            variant="standard"
+            type="password"
+          />
+        </p>
+        <p>
+          <Button class="colorVerde" variant="contained">
+            Ingresar
+          </Button>
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
